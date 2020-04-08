a78646ee7acc06a681477918dadfba97e58c598e
Penelope Maher <p.maher@exeter.ac.uk>
2020-04-08 18:33:27 +0100

    sneaky do_ape flag

--- src/atmos_spectral/driver/solo/mixed_layer.F90
+++ src/atmos_spectral/driver/solo/mixed_layer.F90
@@ -477,7 +477,7 @@ else
 endif
 
 !s begin surface heat capacity calculation
-   if((.not.do_sc_sst).or.(do_sc_sst.and.specify_sst_over_ocean_only).or.(.not.do_ape_sst))then
+   if((.not.do_sc_sst).or.(do_sc_sst.and.specify_sst_over_ocean_only))then
          land_sea_heat_capacity = depth*RHO_CP
     if(trim(land_option) .ne. 'input') then
          if ( trop_capacity .ne. depth*RHO_CP .or. np_cap_factor .ne. 1. ) then !s Lines above make trop_capacity=depth*RHO_CP if trop_capacity set to be < 0.
